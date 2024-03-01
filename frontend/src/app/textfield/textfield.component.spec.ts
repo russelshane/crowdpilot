0b2f6906e4582a43dff02e86a110b9f13f49299f
0.8lili <mashu.rsn@gmail.com>
2024-03-02 00:30:57 +0800

    feat: init textfield component

---
+++ frontend/src/app/textfield/textfield.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TextfieldComponent } from './textfield.component';
+
+describe('TextfieldComponent', () => {
+  let component: TextfieldComponent;
+  let fixture: ComponentFixture<TextfieldComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TextfieldComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TextfieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/textfield/textfield.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textfield',
+  standalone: true,
+  imports: [],
+  templateUrl: './textfield.component.html',
+  styleUrl: '../../styles/components/_textfield.component.scss'
+})
+export class TextfieldComponent {
+
+}
