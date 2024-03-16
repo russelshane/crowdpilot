905326c34b0734f83797940de46399c54d907533
samnario19 <202211449@gordoncollege.edu.ph>
2024-03-16 15:13:10 +0800

    init: user avatar forgot password component

---
+++ web/src/app/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './forgot-password.component.html',
+})
+export class ForgotPasswordComponent {
+
+}
